1dc141721964d4de3e11a5f48004047761e66792
victordiaz86 <victor@remoose.com>
2024-01-02 15:41:10 -0500

    Remove clerk button from home and add to navbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
 function Home() {
   return (
     <div>
-      <h1>Vickoman APP</h1>
-      <div>
-        <UserButton afterSignOutUrl="/" />
-      </div>
+      <div></div>
     </div>
   );
 }
